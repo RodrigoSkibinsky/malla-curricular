e3783da90c2156739b1c9da8df8665e3b0c3b866
Rodrigo Sacramento Skibinsky <80299647+RodrigoSkibinsky@users.noreply.github.com>
2025-08-04 18:02:21 -0300

    Update next.config.tsaaaaaa

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { unoptimized: true },
+  basePath: '/malla-curricular',
+  assetPrefix: '/malla-curricular/',
   experimental: {
     appDir: true,
   },
